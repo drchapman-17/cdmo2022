08a62ce71fb514636c1ded06842b55840d70e88e
Freddavide <93276731+Freddavide@users.noreply.github.com>
2022-07-11 12:34:45 +0200

    Update utils.py

--- models/utils.py
+++ models/utils.py
@@ -275,15 +275,15 @@ def report_barplot_v2(model, save_image = True):
     df['mode'] = 'No rotation'
     df_flip['mode'] = 'Rotation'
 
-    df.loc[df['Time'] > 300, 'mode']= 'Failure no rotation'
-    df_flip.loc[df_flip['Time'] > 300, 'mode']= 'Failure rotation'
+    df.loc[df['Time'] > 300, 'mode']= 'Timeout no rotation'
+    df_flip.loc[df_flip['Time'] > 300, 'mode']= 'Timeout rotation'
 
     df = pd.concat([df, df_flip])
 
     sns.set_palette(sns.color_palette("tab10"))
     plt.figure(figsize = (15,10))
-    sns.barplot(data = df, x = 'Instance', y = 'Time', hue = 'mode', hue_order=['No rotation', 'Failure no rotation'\
-                                                                                ,'Rotation', 'Failure rotation'])
+    sns.barplot(data = df, x = 'Instance', y = 'Time', hue = 'mode', hue_order=['No rotation', 'Timeout no rotation'\
+                                                                                ,'Rotation', 'Timeout rotation'])
 
     plt.xticks(fontsize = 'large')
     plt.yticks(fontsize = 'large')
